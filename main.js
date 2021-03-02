52c6170d6075cb350e1d89a5ad34abbae4a14ac4
Martin <martin.knowles@hotmail.se>
2021-03-02 15:08:36 +0100

    potentiell JS ??

--- main.js
+++ main.js
@@ -8,3 +8,33 @@ function searchSong(song) {
             }
         );
     }
+
+
+        // skall vi göra något sånt här grabbar????
+
+    fetch('spotify api skall vara här' + song + '&appid=' + key + '')
+      .then(function(response) {
+          if (response.ok) {
+              return response.json();
+          } else if (response.status === 400 || response.status === 404) {
+              alert(city + " Ingen låt hittades.")
+          } else if (response.status === 500) {
+              alert("Försök igen.")
+          } else {
+              alert("Försök igen.")
+          }
+      })
+      .then(function(song) {
+          if (document.getElementById("SongBox").checked) {
+              DisplaySong(song)
+          } else if (document.getElementById("AttractionBox").checked) {
+              getAttraction(artist)
+          } else {
+              DisplaySong(song)
+              getAttraction(artist)
+          }
+      })
+      .catch(function(error) {
+          console.log(error.message);
+      });
+}
